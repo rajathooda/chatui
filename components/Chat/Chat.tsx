45200194af05ccc1ed5373fdd2067b6eb315d078
rajathooda <127722895+rajathooda@users.noreply.github.com>
2023-05-04 23:29:55 +0530

    Update Chat.tsx

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,7 +352,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to Gnoo
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Gnoomi excels in intelligent and context driven conversation`}</div>
